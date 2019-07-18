a861b73eabd791a7ccc1c7a5e12f0189d08f86d6
Hung Dao Bui <hungdaobui@gmail.com>
2019-07-18 16:27:51 +0800

    day04

--- workshop04/main.js
+++ workshop04/main.js
@@ -20,7 +20,8 @@ const express = require('express')
 const CitiesDB = require('./citiesdb');
 
 const serviceId = uuid().substring(0, 8);
-const serviceName = `zips-${serviceId}`
+//const serviceName = `zips-${serviceId}`
+const serviceName = 'zips';
 
 //Load application keys
 //Rename _keys.json file to keys.json
@@ -55,13 +56,124 @@ app.use(express.urlencoded({ extended: true }));
 // TODO 2/3 Copy your routes from workshop03 here
 
 
+//Loading the schema file
+const citySchema = require('./schema/city-schema.json');
+
+//comment out to test /health
+
+//new OpenAPIValidator({ 
+//    apiSpecPath: join(__dirname, 'schema', 'city-api.yaml')
+//}).install(app)
+
+// Start of workshop
+// TODO 2/2 Copy your routes from workshop02 here
+
+// Mandatory workshop
+// TODO GET /api/states
+app.get('/api/states', 
+	cacheControl ({ maxAge: 30, private: false}),
+    (req, resp) => {
+
+		console.info('****** GET LIST OF STATES', new Date())
+
+        // Content-Type: appliction/json
+        resp.type('application/json')
+
+        db.findAllStates()
+            .then(result => {
+                // 200 OK
+                resp.status(200)
+                resp.json(result);
+            })
+            .catch(error => {
+                // 400 Bad Request
+                resp.status(400)
+                resp.json({ error: error })
+            });
+    }
+);
+
+// TODO GET /api/state/:state
+app.get('/api/state/:state', 
+    (req, resp) => {
+        const stateAbbrev = req.params.state;
+        resp.type('application/json')
+        db.findAllStates()
+            .then(result => {
+                if (result.indexOf(stateAbbrev.toUpperCase()) < 0) {
+                    resp.status(400);
+                    resp.json({ error: `Not a valid state: '${stateAbbrev}'`})
+                    return;
+                }
+                return (db.findCitiesByState(stateAbbrev))
+            })
+            .then(result => {
+                resp.status(200)
+                resp.json(result.map(v => `/api/city/${v}`));
+            })
+            .catch(error => {
+                // 400 Bad Request
+                resp.status(400)
+                resp.json({ error: error })
+            });
+    }
+);
+
+// TODO GET /api/city/:cityId
+app.get('/api/city/:cityId',
+    (req, resp) => {
+        resp.type('application/json');
+        db.findCityById(req.params.cityId)
+            .then(result => {
+                if (result.length > 0) {
+                    resp.status(200)
+                    resp.json(result[0]);
+                    return
+                }
+                resp.status(404);
+                resp.json({ error: `City not found: ${req.params.cityId}`})
+            })
+            .catch(error => {
+                resp.status(400);
+                resp.json({ error: error});
+            })
+    }
+);
+
+// TODO POST /api/city
+// Content-Type: application/json
+app.post('/api/city', schemaValidator.validate({ body: citySchema }),
+(req, resp) => {
+        const newCity = req.body;
+        resp.type('application/json')
+        db.insertCity(newCity)
+            .then(result => {
+                resp.status(201)
+                resp.json(result);
+            })
+            .catch(error => {
+                resp.status(400);
+                resp.json({ error: error});
+            })
+    }
+)
+
+// Optional workshop
+// TODO HEAD /api/state/:state
+
+
+// TODO GET /state/:state/count
+
+
+
+// TODO GET /city/:name
 
 
 
 // End of workshop
 
 app.get('/health', (req, resp) => {
-	console.info(`health check: ${new Date()}`)
+	console.info(`Health Check at: ${new Date()}`)
 	resp.status(200)
 		.type('application/json')
 		.json({ time: (new Date()).toGMTString() })
@@ -87,6 +199,31 @@ db.getDB()
 			console.info(`\tService id: ${serviceId}`);
 
 			// TODO 3/3 Add service registration here
+			consul.agent.service.register({
+				id: serviceId,
+				name: serviceName,
+				port: PORT,
+				check: {
+					//http: `http://localhost:${PORT}/health`,
+					//interval: '5s',
+					'ttl': '5s', //ttl: time to leave
+					deregistercriticalserviceafter: '10s'
+
+				}
+			})
+
+			//Heart beat
+			setInterval (
+				() => {
+					console.info(serviceId, ': Heartbeat:', new Date());
+					consul.agent.check.pass({
+						id: `service:${serviceId}`
+					})
+
+				},
+				5000 //time in milliseconds
+
+			)
 
 
 
